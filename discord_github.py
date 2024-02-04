e3a0651ccdd2e15ad15f5fe06e8d2ca2be84f81a
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2024-02-04 12:31:44 +1100

    Use Repo dispatch
    Rebuilt to use Repo dispatch with payload variables instead of workflow dispatch.
    
    Allows for a single function to perform multiple actions against the repo.

--- discord_github.py
+++ discord_github.py
@@ -6,11 +6,13 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
+github_repo_url = 'https://api.github.com/repos/MaximumPigs/docker_deploy/dispatches'
 github_token = os.getenv("GITHUB_TOKEN")
 headers = { 'Accept': 'application/vnd.github+json',
-            'Authorization':'token ' + github_token,
-            'Content-Type' : 'application/json' }
-data = '{"ref":"main"}'
+            'Authorization':'Bearer ' + github_token}
+
+actions = [ "start", "stop", "store" ]
+games = [ "palworld", "enshrouded" ]
 
 bot = commands.Bot(command_prefix="!", case_insensitive=True, intents=intents)
 
@@ -20,84 +22,56 @@ async def on_ready():
 
 
 @bot.command()
-async def generate(ctx):
-    options = ["a", "b", "c", "d"]
-    already_sent_res = None  # Safe to remove, used for demo
-
-    await ctx.send(
-        f"{ctx.author.mention}\n"
-        f'Sample question: (**Important**: Type a "!" in front):\n```'
-        "> A\n> B\n> C\n> D```"
-    )
-
-    def check(m):
-        return (
-            m.content.startswith("!")
-            and m.content.lower()[1:] in options
-            and m.channel.id == ctx.channel.id
-        )
-
-    while True:
-        msg = await bot.wait_for("message", check=check)
-        # msg.content now contains the option. You may now proceed to code here
-        # The below code from here are used for demonstration and can be safely removed
-        if already_sent_res is None:
-            sent_message = await ctx.send(
-                f"**{str(msg.author.name)}** has chosen option **{str(msg.content).upper()[1:]}**"
-            )
-            already_sent_res = not None
-        else:
-            await sent_message.edit(
-                f"**{str(msg.author.name)}** has chosen option **{str(msg.content).upper()[1:]}**"
-            )
-
-@bot.command()
 async def get_help(ctx):
 
     await ctx.send(
-        f"{ctx.author.mention}\n"
-        f'Send one of the following messagesType !start to start server\n```'
-        "> !start - Start Server\n> !stop - Stop Server\n> !fetch - Fetch and store game files```"
+        f"{ctx.author.mention}\n\n"
+        f'Syntax\n```'
+        "> !game [ action ] [ game ]```\n"
+        'Example "!game start palworld"\n\n'
+        'Available actions:\n```'
+        "> start  - start game server\n"
+        "> stop   - stop game server\n"
+        "> store  - copy files down from server and store them (runs automatically on stop action)```\n"
+        'Available games:\n```'
+        "> palworld\n"
+        "> enshrouded```"
     )
-
 
 @bot.command()
-async def start(ctx):
+async def game(ctx, action="null", game="null"):
 
-    await ctx.send(
-        f"{ctx.author.mention}\n"
-        f"Starting Palworld Server"
-    )
+    if action != "null" and game != "null":
 
-    github_url = 'https://api.github.com/repos/MaximumPigs/Docker_Deploy/actions/workflows/51003280/dispatches'
+        if game in games:
+            await ctx.send(
+                f"{ctx.author.mention}\n"
+                f"Running {action.lower()} on {game.capitalize()} server"
+            )
 
-    response = requests.post( github_url, headers=headers, data=data )
-    print(response.status_code)
+            json_data = '{"event_type":"' + action.lower() + '","client_payload":{ "game":"' +game.lower() + '" }}'
 
-@bot.command()
-async def fetch(ctx):
+            print(github_repo_url)
+            print(headers)
+            print(json_data)
 
-    await ctx.send(
-        f"{ctx.author.mention}\n"
-        f"Fetching and storing Game Files - will run automatically on server shutdown"
-    )
+            response = requests.post( github_repo_url, headers=headers, data=json_data )
+            print(response.status_code)
+            print(response.reason)
 
-    github_url = 'https://api.github.com/repos/MaximumPigs/Docker_Deploy/actions/workflows/84425606/dispatches'
+        else:
+            await ctx.send(
+                f"{ctx.author.mention}\n"
+                f"Action {action.upper()} or Game {game.upper()} doesn't exist - for a list of actions and games type !get_help"
+            )
+    else:
 
-    response = requests.post( github_url, headers=headers, data=data )
-    print(response.status_code)
-
-@bot.command()
-async def stop(ctx):
-
-    await ctx.send(
-        f"{ctx.author.mention}\n"
-        f"Stopping"
-    )
-
-    github_url = 'https://api.github.com/repos/MaximumPigs/Docker_Deploy/actions/workflows/51003282/dispatches'
-
-    response = requests.post( github_url, headers=headers, data=data )
-    print(response.status_code)
+        await ctx.send(
+            f"{ctx.author.mention}\n\n"
+            f"Action or game not specified.\n\n"
+            'Syntax:\n```'
+            "!game [action] [game]```\n\n"
+            "Type !get_help for more info"
+        )
 
 bot.run(os.getenv("DISCORD_TOKEN"))
